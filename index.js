c381c2267b4f43573478f76cd9a4985dfa243dac
Shufai <techvalaxy@gmail.com>
2023-01-29 15:11:13 -0500

    adding text as costomaer requested

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express()
 const port = 3100;
 
-app.get('/', (req, res) => res.send('Welcome to final git lab!'));
+app.get('/', (req, res) => res.send('Welcome to final git lab this is so tru i love devops !'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
